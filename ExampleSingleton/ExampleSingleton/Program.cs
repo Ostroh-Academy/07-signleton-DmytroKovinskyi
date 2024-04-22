54c0585e53d6b7f483c76d0fe958a7dfdb73a83c
DmytroKovinskyi <163335412+DmytroKovinskyi@users.noreply.github.com>
2024-04-22 15:03:06 +0300

    Example methon Singleton

---
+++ ExampleSingleton/ExampleSingleton/Program.cs
@@ -0,0 +1,46 @@
+﻿using System;
+
+namespace ExampleSingleton
+{
+    public class Singleton
+    {
+        private static Singleton instance;
+
+        private Singleton() { }
+
+        public static Singleton GetInstance()
+        {
+            if (instance == null)
+            {
+                instance = new Singleton();
+            }
+            return instance;
+        }
+
+        public void PrintMessage()
+        {
+            Console.WriteLine("1 2 3");
+        }
+    }
+
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Singleton singletonInstance = Singleton.GetInstance();
+
+            singletonInstance.PrintMessage();
+
+            Singleton anotherSingletonInstance = Singleton.GetInstance();
+
+            if (singletonInstance == anotherSingletonInstance)
+            {
+                Console.WriteLine("Перевірка...");
+            }
+            else
+            {
+                Console.WriteLine("Помилка! Сталася помилка при отриманні Перевірки.");
+            }
+        }
+    }
+}
